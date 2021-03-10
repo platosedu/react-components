1503943d605b25fe275600e6bdf3b7c1f374e1b7
Willy Camargo <camargowilly@gmail.com>
2021-03-10 17:20:44 -0300

    fix: remove from styleguidelist css that does not exist

--- styleguide.config.js
+++ styleguide.config.js
@@ -13,8 +13,7 @@ module.exports = {
   ],
   skipComponentsWithoutExample: true,
   require: [
-    path.join(__dirname, './src/css/reset.scss'),
-    path.join(__dirname, './src/css/tokens.scss'),
+    path.join(__dirname, './src/css/tokens.css'),
     path.join(__dirname, './node_modules/glider-js/glider.css')
   ],
   propsParser: require('react-docgen-typescript').withCustomConfig(
@@ -80,6 +79,7 @@ module.exports = {
                 sourceMap: true
               }
             }
+
           ]
         },
         {
