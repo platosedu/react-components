a46a13cb20d36c1b0e9c1e7e59e27b444b0f3b32
Willy Camargo <camargowilly@gmail.com>
2021-04-19 08:58:08 -0300

    update: FieldText to remove the `textarea` option as `type` property
    The textarea will be moved to another one component

--- src/components/FieldText/index.tsx
+++ src/components/FieldText/index.tsx
@@ -9,7 +9,9 @@ export interface FieldTextProps {
   className?: string
   tabIndex?: number
   value?: string | number
+  defaultValue?: string | number
   label?: string
+  fixedLabel?: boolean
   mask?: string | (string | RegExp)[]
   placeholder?: string
   name?: string
@@ -20,14 +22,7 @@ export interface FieldTextProps {
   required?: boolean
   bordered?: boolean
   error?: string | null
-  type?:
-    | 'text'
-    | 'textarea'
-    | 'password'
-    | 'email'
-    | 'search'
-    | 'number'
-    | 'color'
+  type?: 'text' | 'password' | 'email' | 'search' | 'number' | 'color'
   autocomplete?: 'on' | 'off'
   prefixIcon?: IconSlugsType
   prefixIconPath?: string
@@ -35,191 +30,180 @@ export interface FieldTextProps {
   suffixIcon?: IconSlugsType
   suffixIconPath?: string
   suffixIconComponent?: IconType
-  onChange?: (
-    event:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
-  ) => void
-  onFocus?: (
-    event:
-      | React.FocusEvent<HTMLInputElement>
-      | React.FocusEvent<HTMLTextAreaElement>
-  ) => void
-  onBlur?: (
-    event:
-      | React.FocusEvent<HTMLInputElement>
-      | React.FocusEvent<HTMLTextAreaElement>
-  ) => void
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onFocus?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur?: (event: React.ChangeEvent<HTMLInputElement>) => void
   onPrefixIconClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
   onSuffixIconClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const FieldText: React.FC<FieldTextProps> = ({
-  name,
-  inputId,
-  className,
-  tabIndex,
-  type = 'text',
-  autocomplete = 'on',
-  label = '',
-  placeholder = '',
-  hint,
-  disabled = false,
-  readOnly = false,
-  required = false,
-  bordered = false,
-  error,
-  onChange,
-  onFocus,
-  onBlur,
-  prefixIcon,
-  prefixIconPath,
-  prefixIconComponent,
-  onPrefixIconClick,
-  suffixIcon,
-  suffixIconPath,
-  suffixIconComponent,
-  onSuffixIconClick,
-  mask,
-  value,
-  ...props
-}) => {
-  const isTextarea = type === 'textarea'
-  const [focused, setFocused] = useState(false)
-  const inputIdFormatted = inputId || `field-${name}`
-  const InputElement = isTextarea ? 'textarea' : 'input'
+const FieldText = React.forwardRef<HTMLInputElement, FieldTextProps>(
+  (
+    {
+      name,
+      inputId,
+      className,
+      tabIndex,
+      type = 'text',
+      autocomplete = 'on',
+      label = '',
+      fixedLabel = false,
+      placeholder = '',
+      hint,
+      disabled = false,
+      readOnly = false,
+      required = false,
+      bordered = false,
+      error,
+      onChange,
+      onFocus,
+      onBlur,
+      prefixIcon,
+      prefixIconPath,
+      prefixIconComponent,
+      onPrefixIconClick,
+      suffixIcon,
+      suffixIconPath,
+      suffixIconComponent,
+      onSuffixIconClick,
+      mask,
+      value,
+      defaultValue,
+      ...props
+    },
+    ref
+  ) => {
+    const [focused, setFocused] = useState(false)
+    const inputIdFormatted = inputId || `field-${name}`
 
-  const handleFocus = (
-    e:
-      | React.FocusEvent<HTMLInputElement>
-      | React.FocusEvent<HTMLTextAreaElement>
-  ): void => {
-    if (onFocus) {
-      onFocus(e)
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement>): void => {
+      if (onFocus) {
+        onFocus(e)
+      }
+
+      setFocused(true)
     }
 
-    setFocused(true)
-  }
+    const handleBlur = (e: React.ChangeEvent<HTMLInputElement>): void => {
+      if (onBlur) {
+        onBlur(e)
+      }
 
-  const handleBlur = (
-    e:
-      | React.FocusEvent<HTMLInputElement>
-      | React.FocusEvent<HTMLTextAreaElement>
-  ): void => {
-    if (onBlur) {
-      onBlur(e)
+      setFocused(false)
     }
 
-    setFocused(false)
+    const hasPrefixIcon = Boolean(
+      prefixIcon || prefixIconPath || prefixIconComponent
+    )
+    const hasSuffixIcon = Boolean(
+      suffixIcon || suffixIconPath || suffixIconComponent
+    )
+
+    const containerClassNames = classNames(style.container, className, {
+      [style.isFocused]: Boolean(focused),
+      [style.isBordered]: Boolean(bordered),
+      [style.isFilled]: Boolean(value) || Boolean(defaultValue),
+      [style.isDisabled]: Boolean(disabled),
+      [style.hasError]: Boolean(error),
+      [style.hasLabel]: Boolean(label),
+      [style.isFixedLabel]: fixedLabel,
+      [style.hasPlaceholder]: Boolean(placeholder),
+      [style.hasPrefixIcon]: hasPrefixIcon,
+      [style.hasSuffixIcon]: hasSuffixIcon
+    })
+
+    return (
+      <div className={containerClassNames}>
+        <label className={style.field} htmlFor={inputIdFormatted}>
+          {hasPrefixIcon ? (
+            <Icon
+              size="md"
+              icon={prefixIcon}
+              iconPath={prefixIconPath}
+              iconComponent={prefixIconComponent}
+              className={classNames(style.prefixIcon, {
+                [style.iconClickable]: Boolean(onPrefixIconClick)
+              })}
+              onClick={onPrefixIconClick}
+            />
+          ) : null}
+
+          {mask ? (
+            <InputMask
+              mask={mask}
+              value={value}
+              defaultValue={defaultValue}
+              onChange={onChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              disabled={disabled}
+              readOnly={readOnly}
+              // maskChar={null}
+            >
+              {(inputProps: InputProps): ReactElement => (
+                <input
+                  {...inputProps}
+                  className={style.input}
+                  id={inputIdFormatted}
+                  tabIndex={tabIndex}
+                  type={type}
+                  autoComplete={autocomplete}
+                  name={name}
+                  placeholder={placeholder}
+                  required={required}
+                  onChange={onChange}
+                  // ref={ref}
+                  {...props}
+                />
+              )}
+            </InputMask>
+          ) : (
+            <input
+              className={style.input}
+              id={inputIdFormatted}
+              tabIndex={tabIndex}
+              type={type}
+              autoComplete={autocomplete}
+              name={name}
+              placeholder={placeholder}
+              required={required}
+              value={value}
+              defaultValue={defaultValue}
+              onChange={onChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              disabled={disabled}
+              readOnly={readOnly}
+              ref={ref}
+              {...props}
+            />
+          )}
+
+          {label && (
+            <span className={style.label}>
+              {label}
+              {required ? '*' : null}
+            </span>
+          )}
+
+          {hasSuffixIcon ? (
+            <Icon
+              size="md"
+              icon={suffixIcon}
+              iconPath={suffixIconPath}
+              iconComponent={suffixIconComponent}
+              className={classNames(style.suffixIcon, {
+                [style.iconClickable]: Boolean(onSuffixIconClick)
+              })}
+              onClick={onSuffixIconClick}
+            />
+          ) : null}
+        </label>
+
+        <p className={style.supportText}>{(error ? error : '') || hint}</p>
+      </div>
+    )
   }
-
-  const hasPrefixIcon = Boolean(
-    prefixIcon || prefixIconPath || prefixIconComponent
-  )
-  const hasSuffixIcon = Boolean(
-    suffixIcon || suffixIconPath || suffixIconComponent
-  )
-
-  const containerClassNames = classNames(style.container, className, {
-    [style.error]: Boolean(error),
-    [style.focused]: Boolean(focused),
-    [style.bordered]: Boolean(bordered),
-    [style.filled]: Boolean(value),
-    [style.hasLabel]: Boolean(label),
-    [style.hasPlaceholder]: Boolean(placeholder),
-    [style.disabled]: Boolean(disabled),
-    [style.hasPrefixIcon]: hasPrefixIcon,
-    [style.hasSuffixIcon]: hasSuffixIcon,
-    [style.isTextarea]: isTextarea
-  })
-
-  return (
-    <div className={containerClassNames}>
-      <label className={style.field} htmlFor={inputIdFormatted}>
-        {hasPrefixIcon ? (
-          <Icon
-            size="md"
-            icon={prefixIcon}
-            iconPath={prefixIconPath}
-            iconComponent={prefixIconComponent}
-            className={classNames(style.prefixIcon, {
-              [style.iconClickable]: Boolean(onPrefixIconClick)
-            })}
-            onClick={onPrefixIconClick}
-          />
-        ) : null}
-
-        {mask ? (
-          <InputMask
-            mask={mask}
-            value={value}
-            onChange={onChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            disabled={disabled}
-            readOnly={readOnly}
-            // maskChar={null}
-          >
-            {(inputProps: InputProps): ReactElement => (
-              <input
-                {...inputProps}
-                className={style.input}
-                id={inputIdFormatted}
-                tabIndex={tabIndex}
-                type={type}
-                autoComplete={autocomplete}
-                name={name}
-                placeholder={placeholder}
-                required={required}
-                onChange={onChange}
-                {...props}
-              />
-            )}
-          </InputMask>
-        ) : (
-          <InputElement
-            className={style.input}
-            id={inputIdFormatted}
-            tabIndex={tabIndex}
-            type={type}
-            autoComplete={autocomplete}
-            name={name}
-            placeholder={placeholder}
-            required={required}
-            value={value}
-            onChange={onChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            disabled={disabled}
-            readOnly={readOnly}
-            {...props}
-          />
-        )}
-
-        {label && (
-          <span className={style.label}>
-            {label}
-            {required ? '*' : null}
-          </span>
-        )}
-
-        {hasSuffixIcon ? (
-          <Icon
-            size="md"
-            icon={suffixIcon}
-            iconPath={suffixIconPath}
-            iconComponent={suffixIconComponent}
-            className={classNames(style.suffixIcon, {
-              [style.iconClickable]: Boolean(onSuffixIconClick)
-            })}
-            onClick={onSuffixIconClick}
-          />
-        ) : null}
-      </label>
-
-      <p className={style.supportText}>{(error ? error : '') || hint}</p>
-    </div>
-  )
-}
+)
 
 export default FieldText
